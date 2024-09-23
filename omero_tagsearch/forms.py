9f120c68f69db57e17968a9c43b7f269753edb27
Rémy Dornier <remy.dornier@epfl.ch>
2024-09-23 09:52:48 +0200

    start structuring for multiple dynamic kvp filters

--- omero_tagsearch/forms.py
+++ omero_tagsearch/forms.py
@@ -5,8 +5,9 @@ from django.forms import ChoiceField, RadioSelect
 class TagSearchForm(Form):
     selectedTags = MultipleChoiceField()
     excludedTags = MultipleChoiceField()
-    selectedKeys = MultipleChoiceField()
+    selectedKey0 = ChoiceField()
     selectedNamespaces = MultipleChoiceField()
+    i = 1
 
     operation = ChoiceField(
         widget=RadioSelect,
@@ -21,12 +22,18 @@ class TagSearchForm(Form):
     view_plate = BooleanField(initial=True)
     view_screen = BooleanField(initial=True)
 
+    def set_kvp_field(self, i):
+        selectedKey = ChoiceField()
+        globals()[f"selectedKey{i}"] = selectedKey
+        self.fields[f"selectedKey{i}"] = selectedKey
+
     def __init__(self, tags, keys, namespaces, conn=None, *args, **kwargs):
         super(TagSearchForm, self).__init__(*args, **kwargs)
-
         # Process Tags into choices (lists of tuples)
         self.fields["selectedTags"].choices = tags
         self.fields["excludedTags"].choices = tags
-        self.fields["selectedKeys"].choices = keys
+        self.fields["selectedKey0"].choices = keys
+        self.set_kvp_field(self.i)
+        self.fields[f"selectedKey{self.i}"].choices = keys
         self.fields["selectedNamespaces"].choices = namespaces
         self.conn = conn
